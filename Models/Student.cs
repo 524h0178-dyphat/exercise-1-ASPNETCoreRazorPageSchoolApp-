375dbed53f291b804fe7b8c3672ad87151cf95c0
Phat <duyphathust@gmail.com>
2026-03-31 22:06:11 +0700

    Exer1 Initial

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolAppCore.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
